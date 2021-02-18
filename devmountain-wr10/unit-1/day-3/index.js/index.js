fb992aadce9210dc6cadb463e6ee163e7dd4409a
Carlos Chavez <hamandatorres@gmail.com>
2021-02-18 16:21:54 -0700

    trying

---
+++ devmountain-wr10/unit-1/day-3/index.js/index.js
@@ -0,0 +1,30 @@
+var age = 31
+let age2 = 31
+age2 = 26
+const age3 = 31
+console.log(age2)
+//Declare a variable called showMeSix and give it a value of 3 + 3.  We can perform math operations on numbers.
+var showMeSix = 3+3;
+console.log(showMeSix)
+//Strings
+//Declare a variable called name and give it a value of your name.
+var name = 'Carlos'
+//Declare a variable called greeting and give it a value of a nice greeting.  Use either template strings or concatenation to add your name to the end of the greeting.
+let greeting = 'Good morning ' + name
+console.log(greeting)
+//Booleans
+//Declare a variable called lovesCode and give it a value of a boolean.
+let lovesCode = true
+//Arrays
+//Declare a variable called friends and give it a value of an array containing 3 strings, each one a name of a friend.
+//Console.log the first item in your array
+var friends = ["John", "josh", "Jenna"]
+console.log(0)
+let me = {
+  name,
+  age,
+  lovesCode,
+  friends,
+  greeting
+}
+console.log(me)
---
+++ devmountain-wr10/unit-1/day-3/only.js/only.js
@@ -0,0 +1,5 @@
+var Object = [1, 2, 3, 4, 5]
+Object[1] = Object[4]
+Object[4]
+console.log(Object[1])
+
